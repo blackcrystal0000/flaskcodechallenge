7788eb9cd6c0e78fe836419850182cee6f4f1b09
Crystal <muthonigachanja2000@gmail.com>
2023-09-24 08:32:30 -0700

    Adding class in models.py

--- server/models.py
+++ server/models.py
@@ -1,7 +1,16 @@
 from app import db
 
 class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    address = db.Column(db.String(255), nullable=False)
 
 class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    ingredients = db.Column(db.String(255), nullable=False)
 
 class PizzaRestaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
