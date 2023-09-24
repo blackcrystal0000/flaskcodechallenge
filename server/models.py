d42f750a3748ed5ac055fcac0f4f274f4246c20b
Crystal <muthonigachanja2000@gmail.com>
2023-09-24 08:35:06 -0700

    adding relationships

--- server/models.py
+++ server/models.py
@@ -14,3 +14,4 @@ class PizzaRestaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Float, nullable=False)
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
