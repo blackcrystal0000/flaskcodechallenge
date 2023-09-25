d02972a58c287891914f5f02ea579700a16ed014
Crystal <muthonigachanja2000@gmail.com>
2023-09-25 09:09:12 -0700

    validations

--- server/app.py
+++ server/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models import Restaurant, Pizza, RestaurantPizza
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza_restaurant.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 # Define your routes here
@@ -69,8 +73,52 @@ def get_pizzas():
         })
     return jsonify(formatted_pizzas)
 
+@app.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+
+    # Check if the pizza ID and restaurant ID exist in the database
+    pizza_id = data.get('pizza_id')
+    restaurant_id = data.get('restaurant_id')
+    pizza = Pizza.query.get(pizza_id)
+    if not pizza:
+        return jsonify({'error': 'Pizza not found'}), 404
+    restaurant = Restaurant.query.get(restaurant_id)
+    if not restaurant:
+        return jsonify({'error': 'Restaurant not found'}), 404
+
+    # Create the restaurant-pizza pair
+    try:
+        restaurant_pizza = RestaurantPizza(
+            price=data.get('price'),
+            pizza=pizza,
+            restaurant=restaurant
+        )
+        db.session.add(restaurant_pizza)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({'errors': ['validation errors']}), 400
+
+    return jsonify({
+        'id': pizza_id,
+        'name': pizza.name,
+        'ingredients': pizza.ingredients
+    }), 201
+
 if __name__ == '__main__':
-    from models import Restaurant, Pizza, RestaurantPizza
     db.create_all()
-    app.run(port=5555)
     
+    restaurant1 = Restaurant(name="Dominion Pizza", address="Good Italian, Ngong Road, 5th Avenue")
+    restaurant2 = Restaurant(name="Pizza Hut", address="Westgate Mall, Mwanzi Road, Nrb 100")
+
+    pizza1 = Pizza(name="Cheese", ingredients="Dough, Tomato Sauce, Cheese")
+    pizza2 = Pizza(name="Pepperoni", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
+
+    restaurant_pizza1 = RestaurantPizza(price=5, pizza=pizza1, restaurant=restaurant1)
+    restaurant_pizza2 = RestaurantPizza(price=7, pizza=pizza2, restaurant=restaurant1)
+
+    db.session.add_all([restaurant1, restaurant2, pizza1, pizza2, restaurant_pizza1, restaurant_pizza2])
+    db.session.commit()
+    
+    app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from app import db
+from sqlalchemy.orm import validates
 
 class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,6 +7,12 @@ class Restaurant(db.Model):
     address = db.Column(db.String(255), nullable=False)
     pizzas = db.relationship('Pizza', secondary='restaurant_pizza', back_populates='restaurants')
     restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant')
+
+    @validates('name')
+    def validate_name(self, key, value):
+        if len(value.strip()) > 50:
+            raise ValueError("Restaurant name must not be more than 50 words.")
+        return value
 
 class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,3 +28,9 @@ class RestaurantPizza(db.Model):
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
     pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
     restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+
+    @validates('price')
+    def validate_price(self, key, value):
+        if value < 1 or value > 30:
+            raise ValueError("Price must be between 1 and 30.")
+        return value
